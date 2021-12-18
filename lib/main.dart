3bbd8cae412a38c9d6fd148d1ef115773f450b36
susumOyaji <sumitomo0210@gmail.com>
2021-12-18 11:00:19 +0900

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:google_ml_kit/google_ml_kit.dart';  
+import 'package:google_ml_kit/google_ml_kit.dart';
+import './secondpage.dart';  
 
 
 void main() {
@@ -16,15 +17,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -35,23 +27,8 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
-  final image = await _controller.takePicture();
-  final inputimage = await InputImage.fromFilePath(image.path);
-
-
-
-
+  
 
   @override
   State<MyHomePage> createState() => TakePictureScreenState();
@@ -60,7 +37,13 @@ class MyHomePage extends StatefulWidget {
 
 
 class TakePictureScreenState extends State<MyHomePage> {
+  final image = await _controller.takePicture();
+  final inputimage = await InputImage.fromFilePath(image.path);
+
+  
   late CameraController _controller;
+
+
   late Future<void> _initializeControllerFuture;
 
   FaceDetector faceDetector =
@@ -71,9 +54,63 @@ class TakePictureScreenState extends State<MyHomePage> {
 
 
 
+
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        // Provide an onPressed callback.
+        onPressed: () async {
+          // 省略
+          // 笑顔度を受け取ってセットする
+          var result = "";
+          await detectFace(inputimage).then((String value) {
+            result = value;
+            print("value : $value");
+          });
+
+          // If the picture was taken, display it on a new screen.
+          await Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => DisplayPictureScreen(
+                // Pass the automatically generated path to
+                // the DisplayPictureScreen widget.
+                imagePath: image.path,
+                smileText: result,
+              ),
+            ),
+          );
+        },//onPressed
+      ),
+    );
+  }
+  }
+
+
+
+
   Future detectFace(InputImage inputImage) async {
   final faces = await faceDetector.processImage(inputImage);
+  String resText = "No Face";
   print('Found ${faces.length} faces');
+
 
   if (faces.length != 0) {
     for (Face face in faces) {
@@ -89,6 +126,10 @@ class TakePictureScreenState extends State<MyHomePage> {
       if (face.smilingProbability != null) {
         final double? smileProb = face.smilingProbability;
         print("Smile Prob : $smileProb");
+        //笑顔度を返す
+        int intsmileProb = (smileProb! * 100).round();
+        resText = "SMILE $intsmileProb %";
+
       }
 
       // If face tracking was enabled with FaceDetectorOptions:
@@ -98,103 +139,45 @@ class TakePictureScreenState extends State<MyHomePage> {
       }
     }
   }
-}
-
-
-Future<String> detectFace(InputImage inputImage) async {
-  final faces = await faceDetector.processImage(inputImage);
-  String resText = "No Face";
-  print('Found ${faces.length} faces');
-
-  // 省略
-      // If classification was enabled with FaceDetectorOptions:
-      if (face.smilingProbability != null) {
-        final double? smileProb = face.smilingProbability;
-        print("Smile Prob : $smileProb");
-        //笑顔度を返す
-        int intsmileProb = (smileProb! * 100).round();
-        resText = "SMILE $intsmileProb %";
-      }
-  // 省略
-  }
   return resText;
 }
 
 
 
+// A widget that displays the picture taken by the user.
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+  final String smileText;
 
-
-
-}
-
-
-
-
-
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const DisplayPictureScreen(
+      {Key? key, required this.imagePath, required this.smileText})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: AppBar(title: const Text('Display the Picture'),
+          backgroundColor: Colors.blue
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      body: Stack(children: [
+        Image.file(File(imagePath)),
+        Center(
+          child: Container(
+            alignment: Alignment.bottomCenter,
+            margin: EdgeInsets.all(30.0),
+            child: Text(
+              smileText,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50.0,
+                  color: Colors.deepOrangeAccent),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+        )
+      ], fit: StackFit.expand),
     );
   }
 }
+
